6e7ef24cb74de313e1ddca3b098becd928cdf551
Ugo Tanielian <u.tanielian@criteo.com>
2021-12-17 11:21:20 +0000

    2nd commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## The goal of this project is to recommend movies based on the famous 1000 rankings by Sight and Sound 
+The list can be found here: https://www.imdb.com/list/ls081703490/.
+
+
+### Set up
+pip install --upgrade pip
+pip install -e .
+
+### How to use it
+python movie_recommendations/movie_recommendations --movie_name NAME --movie_year YEAR
--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     
     setup(
-      name = 'movie_recommendation_sight_and_sound',
-      packages = find_packages(),
-      include_package_data = True,
-      version = '1',
-      license='MIT',
-      description = 'Recommending movies based on text synopsis using DistilBERT',
-      authors = 'Ugo Tanielian',
-      url = 'https://github.com/tanouch/synthetic_gans',
-      keywords = ['NLP', 'movies'],
-      install_requires=[
+        name = 'recoMovies_SightSound',
+        #package_dir={"": "movie_recommendations"},
+        #packages=find_packages(where="movie_recommendations"),
+        scripts=['movie_recommendations/movie_recommendations.py'],
+        include_package_data = True,
+        version = '1',
+        license='MIT',
+        description = 'Recommending movies based on text synopsis using DistilBERT',
+        author = 'Ugo Tanielian',
+        author_email = 'firstname.lastname@outluuk.fr',  
+        url = 'https://github.com/tanouch/recommending_movies',
+        entry_points={"console_scripts": ["recoMovies_SightSound = movie_recommendations:get_movie_recommendations"]},
+        keywords = ['NLP', 'movies'],
+        install_requires=[
             "numpy",
             "wikipedia",
             "transformers",
@@ -20,11 +24,11 @@ if __name__ == "__main__":
             "fire",
             "scipy"
         ],
-      classifiers=[
+        classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-      ],
+        ]
     )
