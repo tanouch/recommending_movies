e05eb97484274c4d8f5f33c3d5a2053c6bfd1144
tanouch <ugo.tanielian@outlook.fr>
2022-07-25 12:45:46 +0200

    Update main.py

--- recommending_movies_gcloud/main.py
+++ recommending_movies_gcloud/main.py
@@ -2,16 +2,26 @@ from flask import escape
 from reco_movies.movie_recommendations import get_movie_recommendations
 
 def recommending_movies(request):
-    """Responds to any HTTP request.
+    """HTTP Cloud Function.
     Args:
-        request (flask.Request): HTTP request object.
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
     request_json = request.get_json()
-    movie_name = request_json["movie_name"]
-    movie_year = request_json["movie_year"]
+    request_args = request.args
+
+    if request_json:
+        movie_name = request_json["movie_name"]
+        movie_year = request_json["movie_year"]
+    elif request_args:
+        movie_name = request_args["movie_name"]
+        movie_year = request_args["movie_year"]
+    else:
+        movie_name = "The Godfather"
+        movie_year = 1972
     movie_reco_string = get_movie_recommendations(movie_name, movie_year)
     return '{}'.format(escape(movie_reco_string))
