16c69aa263f54b0824e93dcbf6a83b38f5a9db77
tanouch <ugo.tanielian@outlook.fr>
2022-10-21 11:32:21 +0200

    Removing CORS tricks in main.py

--- recommending_movies_gcloud/main.py
+++ recommending_movies_gcloud/main.py
@@ -12,23 +12,25 @@ def recommending_movies(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
-        }
 
-        return ('', 204, headers)
+    # # Set CORS headers for the preflight request
+    # if request.method == 'OPTIONS':
+    #     # Allows GET requests from any origin with the Content-Type
+    #     # header and caches preflight response for an 3600s
+    #     headers = {
+    #         'Access-Control-Allow-Origin': '*',
+    #         'Access-Control-Allow-Methods': 'GET',
+    #         'Access-Control-Allow-Headers': 'Content-Type',
+    #         'Access-Control-Max-Age': '3600'
+    #     }
 
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*'
-    }
+    #     return ('', 204, headers)
+
+    # # Set CORS headers for the main request
+    # headers = {
+    #         'Access-Control-Allow-Origin': '*',
+    #         'Access-Control-Allow-Methods': 'POST',
+    #     }
 
     request_json = request.get_json()
     request_args = request.args
@@ -43,4 +45,5 @@ def recommending_movies(request):
         movie_name = "The Godfather"
         movie_year = 1972
     movie_reco_string = get_movie_recommendations(movie_name, movie_year)
-    return ('{}'.format(escape(movie_reco_string)), 200, headers)
+    return '{}'.format(escape(movie_reco_string))
+    #return ('{}'.format(escape(movie_reco_string)), 200, headers)
